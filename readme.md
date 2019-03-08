a457f087c66f476e1ffea7520fc5aea9db848586
sgsultana <sgsultana@gmail.com>
2019-03-08 16:24:44 -0600

    Added assignment files and description

---
+++ readme.md
@@ -0,0 +1,8 @@
+Part 3: Ch. 10 Programming Exercise #5
+RetailItem Class
+
+Write a class named RetailItem that holds data about an item in a retail store. The class should store 
+the following data in attributes: item description, units in inventory, and price.
+
+Use the attached program wk4InClassPart3.py that creates three RetailItem objects to test your class. 
+The program should store this data in the three objects, then display the data for each item on the screen.
---
+++ wk4InClassPart3.py
@@ -0,0 +1,24 @@
+# Programming Exercise 10-5
+
+import retail
+
+def main():
+    # Create three instances of RetailItem.
+    item1 = retail.RetailItem('Jacket', 12, 59.95)
+    item2 = retail.RetailItem('Designer Jeans', 40, 34.95)
+    item3 = retail.RetailItem('Shirt', 20, 24.95)
+
+
+    # Display information.
+    print ('Retail Item 1: ')
+    print (item1)
+    print()
+    print ('Retail Item 2:')
+    print (item2)
+    print()
+    print ('Retail Item 3:')
+    print (item3)
+
+# Call the main function.
+main()
+
